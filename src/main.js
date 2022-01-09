085748b51401229ae90e8b618beec342a1604b06
Arthur <347771054@qq.com>
2022-01-09 15:03:49 +0800

    按需引入ElIcon

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import {createApp} from 'vue';
 import App from './App.vue';
+import {ElIcon} from 'element-plus';
+import 'element-plus/dist/index.css';
 
 import router from './routes';
 
 const app = createApp(App);
 
-app.use(router);
+app.use(router)
+   .use(ElIcon);
 app.mount('#app');
